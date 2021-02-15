c2c8bb1a25a99030f287854232bede038971a189
Saurabh <sharmarsaurabh@gmail.com>
2021-02-15 23:17:39 +0530

    gitdemo person commit

--- src/test/java/projects/fox/tests/Loginmemsql.java
+++ src/test/java/projects/fox/tests/Loginmemsql.java
@@ -7,5 +7,8 @@ public class Loginmemsql {
 	String url = "jdbc:mysql://svc-2c2023e1-4245-4bf5-b2c9-1b9e4b825957-dml.aws-oregon-1.db.memsql.com:3306/fox_ai_instrumentation";
 	String user = "rdusr_serelium";
 	String pass = "f9HFA3zqcAkh";
-
+	
+	String fname = "Saurabh";
+	String lname = "Sharma";
+	int age = 33;
 }
