9213116b038d3298a0e722143f8e4299c49f7dcd
Saurabh <sharmarsaurabh@gmail.com>
2021-02-15 23:48:50 +0530

    done on develop brnach by gitdemo person

--- src/test/java/projects/fox/tests/Loginmemsql.java
+++ src/test/java/projects/fox/tests/Loginmemsql.java
@@ -10,4 +10,17 @@ public class Loginmemsql {
 	
 	float a[];
 	char asr[];
+	
+	String url2 = "jdbc:mysql://svc-2c2023e1-4245-4bf5-b2c9-1b9e4b825957-dml.aws-oregon-1.db.memsql.com:3306/fox_ai_instrumentation";
+	String user2 = "rdusr_serelium";
+	String pass3 = "f9HFA3zqcAkh";
+	
+	public void  returnurl2()
+	{
+		System.out.println(url2);
+	}
+	public void  returnurl1()
+	{
+		System.out.println(url);
+	}
 }
